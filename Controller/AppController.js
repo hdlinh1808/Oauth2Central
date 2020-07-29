ce68d15253076f5e220223bbc5fc07808eca2f6e
linhhd <linhhd@vng.com.vn>
2020-07-29 21:36:29 +0700

    update

--- Controller/AppController.js
+++
@@ -1,7 +0,0 @@
-const DAOImplObject = require('../DAOImpl/DAOImplObject.js')
-class AppController {
-    constructor() {
-        this.appDaoImpl = DAOImplObject.getAppDaoImpl();
-    }
-}
-module.exports = AppController;
--- entity/App.js
+++
@@ -1,9 +0,0 @@
-class App {
-    constructor(name, imageUrl, baseUrl, users){
-        this.name = name;
-        this.imageUrl = imageUrl;
-        this.baseUrl = baseUrl;
-        this.users = users;
-    }
-}
-module.exports = App
